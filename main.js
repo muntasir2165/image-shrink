7c625cf8d7d8ed0eb33cfc77f6e32748c8282d7d
Muntasir Biojid <muntasir2165@hotmail.com>
2020-06-08 14:41:56 -0700

    v-12 creating a menu template

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 // Set env
 process.env.NODE_ENV = 'development';
@@ -21,7 +21,22 @@ function createMainWindow() {
   mainWindow.loadFile('./app/index.html');
 }
 
-app.on('ready', createMainWindow);
+app.on('ready', () => {
+  createMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on('closed', () => (mainWindow = null));
+});
+
+const menu = [
+  ...(isMac ? [{ role: 'appMenu' }] : []),
+  {
+    label: 'File',
+    submenu: [{ label: 'Quit', click: () => app.quit() }],
+  },
+];
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
