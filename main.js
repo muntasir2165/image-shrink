e3dd40c99de33a9071d41e6491b697de85db0418
Muntasir Biojid <muntasir2165@hotmail.com>
2020-06-07 23:00:49 -0700

    v-9 changed icon path to absolute to avoid an error when packaging

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function createMainWindow() {
     title: 'ImageShrink',
     width: 500,
     height: 600,
-    icon: './assets/icons/Icon_256x256.png',
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
   });
 
   // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
