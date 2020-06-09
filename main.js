9699f8638d9b77314ede7f22df7b5383fad849f4
Muntasir Biojid <muntasir2165@hotmail.com>
2020-06-08 18:16:18 -0700

    v-14 menu roles

--- main.js
+++ main.js
@@ -39,11 +39,21 @@ app.on('ready', () => {
 const menu = [
   ...(isMac ? [{ role: 'appMenu' }] : []),
   {
-    label: 'File',
-    submenu: [
-      { label: 'Quit', accelerator: 'CmdOrCtrl+W', click: () => app.quit() },
-    ],
+    role: 'fileMenu',
   },
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' },
+          ],
+        },
+      ]
+    : []),
 ];
 
 // Quit when all windows are closed.
