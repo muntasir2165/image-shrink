925a975beb77cca38b4797ef8699d7dff8e33e2f
Muntasir Biojid <muntasir2165@hotmail.com>
2020-06-08 18:29:48 -0700

    v-15 open window from menu

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu, globalShortcut } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 // Set env
 process.env.NODE_ENV = 'development';
@@ -7,6 +7,7 @@ const isDev = process.env.NODE_ENV !== 'production' ? true : false;
 const isMac = process.platform === 'darwin' ? true : false;
 
 let mainWindow;
+let aboutWindow;
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
@@ -22,25 +23,47 @@ function createMainWindow() {
   mainWindow.loadFile('./app/index.html');
 }
 
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    title: 'About ImageShrink',
+    width: 300,
+    height: 300,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: false,
+    backgroundColor: 'white',
+  });
+
+  aboutWindow.loadFile('./app/about.html');
+}
+
 app.on('ready', () => {
   createMainWindow();
 
-  globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload());
-  globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () =>
-    mainWindow.toggleDevTools()
-  );
-
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
-
   mainWindow.on('closed', () => (mainWindow = null));
 });
 
 const menu = [
-  ...(isMac ? [{ role: 'appMenu' }] : []),
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [{ label: 'About', click: createAboutWindow }],
+        },
+      ]
+    : []),
   {
     role: 'fileMenu',
   },
+  ...(!isMac
+    ? [
+        {
+          label: 'Help',
+          submenu: [{ label: 'About', click: createAboutWindow }],
+        },
+      ]
+    : []),
   ...(isDev
     ? [
         {
