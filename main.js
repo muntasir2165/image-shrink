e1b242c35eccf8a6cef8f314eb1cedc69441c965
Muntasir Biojid <muntasir2165@hotmail.com>
2020-06-09 21:43:06 -0700

    v-18 ipc communication

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 
 // Set env
 process.env.NODE_ENV = 'development';
@@ -86,6 +86,10 @@ const menu = [
       ]
     : []),
 ];
+
+ipcMain.on('image:minimize', (e, options) => {
+  console.log(options);
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
