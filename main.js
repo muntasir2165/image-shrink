1a4969bd1e5533281547e36943c8b4faf5540efc
Muntasir Biojid <muntasir2165@hotmail.com>
2020-06-10 18:59:12 -0700

    v-21 creating log files

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ const imagemin = require('imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminPngquant = require('imagemin-pngquant');
 const slash = require('slash');
+const log = require('electron-log');
 
 // Set env
-process.env.NODE_ENV = 'development';
-// process.env.NODE_ENV = 'production';
+// process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'production';
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false;
 const isMac = process.platform === 'darwin' ? true : false;
@@ -110,13 +111,13 @@ async function shrinkImage({ imgPath, quality, dest }) {
       ],
     });
 
-    console.log(files);
+    log.info(files);
 
     shell.openPath(dest);
 
     mainWindow.webContents.send('image:done');
   } catch (err) {
-    console.log(err);
+    log.error(err);
   }
 }
 
